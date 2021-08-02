e404b4624e8d8b6dcfbd59ceca9678d092ea7220
ruancorreia <correia.ruan@gmail.com>
2021-08-02 13:05:23 -0300

    alteraçoes cabeçalho

--- routes/produtos.js
+++ routes/produtos.js
@@ -5,9 +5,10 @@ routes.get("/produtos/lista", (req, res) =>{
     res.send("pagina de produtos")
 });
 
-routes.get("/produto/:nome", (req, res) =>{  // (:nome) rota parametrizada de pesquisa de termo digitado
+routes.get("/produto/:nome?", (req, res) =>{  // (:nome) rota parametrizada de pesquisa de termo digitado
     console.log(req.params.nome);              //como se fosse uma variavel que recebe o dado digitado
     res.send("pagina de produtos")
 });
+
 
 module.exports = routes;
